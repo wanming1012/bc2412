afef2cc53d013b8f25b3a431f5a1a711b9e65894
wanming1012 <wanming1012@gmail.com>
2024-12-10 12:42:54 +0800

    20241210 exercise 1

---
+++ demo-helloworld/src/Exercise20241210.java
@@ -0,0 +1,11 @@
+public class Exercise20241210 {
+  public static void main(String[] args) {
+    double priceForApple = 7.3;
+    double priceForOrange = 6.5;
+    int quantityOfApple = 3;
+    int quantityOfOrange = 4;
+
+    double totalAmount = priceForApple * quantityOfApple + priceForOrange * quantityOfOrange;
+    System.out.println(totalAmount);
+  }
+}
---
+++ demo-helloworld/src/Helloworld.java
@@ -0,0 +1,16 @@
+public class Helloworld {
+    public static void main(String[] args) {
+        int x = 3;
+        int dayOfWeek = 7;
+        double y2 = 3.2;
+        x = 101;
+        x = x + 10;
+        x = x - 40;
+        x = x * 3;
+        x = x / 213;
+
+        double x1 = 10.0 / 3.0;
+
+        System.out.println(x1);
+    }
+}
